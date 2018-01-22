925aaa40deb904110af222a69d0e2c8cffd80d61
Yulyulk <Chananchida>
2018-01-22 22:58:53 +0700

    first commit

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
 import socket
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-MESSAGE = "Hello, World!"
+UDP_PORT = 5030
+MESSAGE = "5"
 
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
@@ -11,3 +11,7 @@ print "message:", MESSAGE
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+while True:
+    data, addr = sock.recvfrom(1024)
+    print data
+
--- server.py
+++ server.py
@@ -1,9 +1,20 @@
 import socket
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_PORT = 5030
+
+def fact(n):
+  f = 1
+  for i in range(1, n +1):
+   f *= i
+  return f
+
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+    s = fact(int (data))
+    print "received message:", s
+    d = str(s)
+    sock.sendto(d, addr)   
+    
